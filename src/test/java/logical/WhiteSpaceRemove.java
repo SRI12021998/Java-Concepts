5d18e2e6867ce79a00791ab8efc8554aa0aa8a3a
SRI12021998 <sriram12021998@gmail.com>
2025-06-04 17:39:25 +0530

    Logic included

--- src/test/java/logical/WhiteSpaceRemove.java
+++ src/test/java/logical/WhiteSpaceRemove.java
@@ -5,7 +5,8 @@ public class WhiteSpaceRemove
 
 	public static void main(String[] args) 
 	{
-		
+		String input="This is a space removal testing";
+		System.out.println(input.replaceAll(" ",""));
 	}
 
 }
